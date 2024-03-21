d6d0ae350f63415f45d8bc855db4ccefbf584466
Luigi Consentino <luigi.aconsentino@gmail.com>
2024-03-20 21:59:59 -0300

    feat: criação do layout + lógica da adição de ml ingerido no dia

--- app/src/main/java/com/example/projeto_aguaponto/MainActivity.java
+++ app/src/main/java/com/example/projeto_aguaponto/MainActivity.java
@@ -1,4 +1,51 @@
 package com.example.projeto_aguaponto;
 
-public class MainActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.projeto_aguaponto.model.AdicionarMl;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    //definição das variáveis
+    private Button bt_adicionar_agua;
+    private AdicionarMl adicionarMl;
+    private TextView txt_resultado_ml_main;
+
+    private int resultado_ml_atual = 0;
+    private int adicionar_agua = 300;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //Criação do metodo que agrupa a lógica
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //referencia ao activity main
+
+        IniciarComponentes(); //chamando a função que inicia os componentes *foi criado no fim do código
+
+        AdicionarMl adicionarMl = new AdicionarMl(); //Criação do objeto adicionarMl
+
+        bt_adicionar_agua.setOnClickListener(new View.OnClickListener() { //Referenciando o botão que será clicado
+            @Override
+            public void onClick(View v) { //metodo que executa uma ação ao clicar no botão
+
+                adicionarMl.Somar(resultado_ml_atual, adicionar_agua);
+                resultado_ml_atual = adicionarMl.ResultadoMlAtual();
+                NumberFormat formatar = NumberFormat.getNumberInstance(new Locale("pt", "BR"));
+                formatar.setGroupingUsed(false);
+                txt_resultado_ml_main.setText(formatar.format(resultado_ml_atual));
+            }
+        });
+
+    }
+    public void IniciarComponentes(){ //criação do metodo de iniciar componentes
+        bt_adicionar_agua = findViewById(R.id.bt_add_ingestao);
+        txt_resultado_ml_main = findViewById(R.id.txt_resultado_ml_main);
+    }
 }
